3fafaedfb1e54d408e3241fa2310e671a13c43c6
Nicholas Welna <welnanick@gmail.com>
2018-04-18 14:33:17 -0700

    T0X.05-Exercise-SilentMode: Finished

--- app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
@@ -1,25 +1,37 @@
 package com.example.android.shushme;
 
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.os.Build;
+import android.os.Build.VERSION;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 
 public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
@@ -27,7 +39,8 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
     /***
      * Handles the Broadcast message sent when the Geofence Transition is triggered
-     * Careful here though, this is running on the main thread so make sure you start an AsyncTask for
+     * Careful here though, this is running on the main thread so make sure you start an
+     * AsyncTask for
      * anything that takes longer than say 10 second to run
      *
      * @param context
@@ -35,14 +48,105 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
      */
     @Override
     public void onReceive(Context context, Intent intent) {
+
         Log.i(TAG, "onReceive called");
-        // TODO (4) Use GeofencingEvent.fromIntent to retrieve the GeofencingEvent that caused the transition
+        GeofencingEvent event = GeofencingEvent.fromIntent(intent);
 
-        // TODO (5) Call getGeofenceTransition to get the transition type and use AudioManager to set the
-        // phone ringer mode based on the transition type. Feel free to create a helper method (setRingerMode)
+        int geofenceTransition = event.getGeofenceTransition();
 
-        // TODO (6) Show a notification to alert the user that the ringer mode has changed.
-        // Feel free to create a helper method (sendNotification)
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+
+            setRingerMode(context, AudioManager.RINGER_MODE_SILENT);
+
+        }
+        else if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            setRingerMode(context, AudioManager.RINGER_MODE_NORMAL);
+
+        }
+        else {
+
+            Log.e(TAG, String.format("Unknown transition : %d", geofenceTransition));
+            return;
+
+        }
+
+        sendNotification(context, geofenceTransition);
 
     }
+
+    private void sendNotification(Context context, int transitionType) {
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+
+        // Construct a task stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // Get a PendingIntent containing the entire back stack.
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel mChannel =
+                    new NotificationChannel("geofence-alert", "Geofence Alerts",
+                            NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        // Get a notification builder
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(context, "geofence-alert");
+
+        // Check the transition type to display the relevant icon image
+        if (transitionType == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            builder.setSmallIcon(R.drawable.ic_volume_off_white_24dp).setLargeIcon(BitmapFactory
+                    .decodeResource(context.getResources(), R.drawable.ic_volume_off_white_24dp))
+                   .setContentTitle(context.getString(R.string.silent_mode_activated));
+        }
+        else if (transitionType == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            builder.setSmallIcon(R.drawable.ic_volume_up_white_24dp).setLargeIcon(BitmapFactory
+                    .decodeResource(context.getResources(), R.drawable.ic_volume_up_white_24dp))
+                   .setContentTitle(context.getString(R.string.back_to_normal));
+        }
+
+        // Continue building the notification
+        builder.setContentText(context.getString(R.string.touch_to_relaunch));
+        builder.setContentIntent(notificationPendingIntent);
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
+
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build());
+    }
+
+    private void setRingerMode(Context context, int mode) {
+
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (VERSION.SDK_INT < 24 || (VERSION.SDK_INT >= 24 &&
+                !notificationManager.isNotificationPolicyAccessGranted())) {
+
+            AudioManager audioManager =
+                    (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setRingerMode(mode);
+
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/android/shushme/MainActivity.java
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -16,16 +16,20 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.app.NotificationManager;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build.VERSION;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -246,14 +250,33 @@ public class MainActivity extends AppCompatActivity implements
             locationPermissions.setEnabled(false);
         }
 
-        //TODO (3) Initialize ringer permissions checkbox
-    }
+        CheckBox ringerPermissions = findViewById(R.id.ringer_permission_checkbox);
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-    // TODO (2) Implement onRingerPermissionsClicked to launch ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS
+        if (VERSION.SDK_INT >= 24 && !nm.isNotificationPolicyAccessGranted()) {
+
+            ringerPermissions.setChecked(false);
+
+        }
+        else {
+
+            ringerPermissions.setChecked(true);
+            ringerPermissions.setEnabled(false);
+
+        }
+
+    }
 
     public void onLocationPermissionClicked(View view) {
         ActivityCompat.requestPermissions(MainActivity.this,
                 new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                 PERMISSIONS_REQUEST_FINE_LOCATION);
+    }
+
+    public void onRingerPermissionClicked(View view) {
+
+        Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+        startActivity(intent);
+
     }
 }
